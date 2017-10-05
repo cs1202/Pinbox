608d51329e14bfd6816ecd71b1ef869a7ec83730
Michael Yawanis <michael@dhcp-10-102-220-58.wireless.northwestern.private>
2017-10-05 12:00:12 -0500

    starter files

---
+++ assets/javascript/script.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCXCHm7-dGstFOKYEcyg3U5RoOccCZvz34",
+    authDomain: "pinbox-3fdd8.firebaseapp.com",
+    databaseURL: "https://pinbox-3fdd8.firebaseio.com",
+    projectId: "pinbox-3fdd8",
+    storageBucket: "pinbox-3fdd8.appspot.com",
+    messagingSenderId: "335499738606"
+  };
+  firebase.initializeApp(config);
