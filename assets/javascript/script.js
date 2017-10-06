f29aaf30c47da4dbb27f0c345fcd03dfb25acefc
Scott Foster <thescottfoster@gmail.com>
2017-10-06 13:54:27 -0500

    added search results to DOM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,30 +18,92 @@ firebase.initializeApp(config);
 
 // --- --- GLOBAL FUNCTIONS --- ---
 
+
+//function to take a string and execute an AJAX search, returning the JSON search object
 function getSearchResults( searchQuery ){
 
     console.log ( 'Search Query: ' +searchQuery );
 
+    //create URL for query
     
+    var queryURL = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD6bIuTTEm5v6yAgiJ2HWopoC0OS0q22XY&cx=008323734482261316548:7dymxyxvecs&q=' +searchQuery;
+
+    $.ajax({ url: queryURL, method: "GET" }).done(function(response) {
+
+    console.log(response);
+
+    makeObjects(response);
+
+    });
+
+}
+
+function makeObjects( ajaxResponse ){
+
+//replace with for loop
+    var i = 0;
+
+    var searchResultObject = {
+                                title: ajaxResponse.items[i].title, 
+                                link: ajaxResponse.items[i].link,
+                                snippet: ajaxResponse.items[i].snippet
+                            };
+
+    console.log(searchResultObject);
+
+    displayResults(searchResultObject);
+
+}
+
+
+// prepend object to DOM
+function displayResults( objectToDisplay ){
+
+    //create a div
+    //append title
+    //append link as a hyperlink
+    //append snippet 
+    $('#search-results-shown').append(
+        `
+        <div>
+            <h3>${objectToDisplay.title}</h3>
+                    
+            <a href="${objectToDisplay.link}" target="_blank">${objectToDisplay.link}</a>
+
+            <p>${objectToDisplay.snippet}</p>
+
+            <hr>
+        </div>
+
+        `
+    );
+
+
 
 }
 
 
 
+
+
 // --- --- EVENT HANDLERS --- ---
 
+// when search form submitted (click or enter)
 
 $('#search').submit( function( event ){
 
     event.preventDefault();
 
+    //retrieve the contents of the input box
     inputText = $('#search-input').val();
 
     console.log( 'Input Text: ' + inputText );
 
+    //initiate a search using the search term
     getSearchResults( inputText );
 
 });
+
 
 
 
